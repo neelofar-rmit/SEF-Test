49b47cce00b3af4a0c155db7411a7ac1488daa01
neelofar <neelofar@localhost>
2025-05-29 20:01:06 +1000

    first commit

---
+++ tutorial/src/test/java/tutorial/BookTest.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
